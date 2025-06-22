660fa114daebed7953a552b39a14b1a70f6474c4
pranav89624 <pranav12340987@gmail.com>
2025-06-22 16:29:03 +0530

    Add Navbar

---
+++ constants/index.js
@@ -0,0 +1,20 @@
+const navLinks = [
+ {
+	id: "tea",
+	title: "Teas",
+ },
+ {
+	id: "about",
+	title: "About Us",
+ },
+ {
+	id: "work",
+	title: "The Art",
+ },
+ {
+	id: "contact",
+	title: "Contact",
+ },
+];
+
+export { navLinks };
